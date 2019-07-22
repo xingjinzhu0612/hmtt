cd2012315225870679a8034f2d0193ebdff5bd76
xingjinzhu0612 <1018715459@qq.com>
2019-07-22 11:48:10 +0800

    首页

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/home/home.vue'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-
+    { name: 'home', path: '/', component: Home }
   ]
 })
 export default router
