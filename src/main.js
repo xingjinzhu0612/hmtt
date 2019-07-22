f2b3d46754ddebb2c9c57687b5cef187169107ce
xingjinzhu0612 <1018715459@qq.com>
2019-07-22 11:26:45 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import router from './router/router'
+import axios from 'axios'
+axios.prototype.axios = axios
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,65 +1,109 @@
 <template>
-    <div class="login-container">
+  <div class="login-container">
     <el-card class="login">
-        <img src="../../assets/images/logo_index.png" alt="">
-        <el-form>
-            <el-form-item>
-                <el-input></el-input>
-            </el-form-item>
-             <el-form-item>
-                <el-input style="width:260px"></el-input>
-                <el-button style="float:right">验证码</el-button>
-            </el-form-item>
-            <el-form-item class="checkbox">
-                <el-checkbox v-model="checked"></el-checkbox>
-                <span>我已阅读并同意 <el-link type="primary" :underline="false">用户协议</el-link>和<el-link type="primary" :underline="false">隐私条款</el-link></span>
-            </el-form-item>
-            <el-form-item>
-               <el-button style="width:100%" type="primary" >登录</el-button>
-            </el-form-item>
-        </el-form>
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form  :model="loginForm" :rules="loginRules" ref="loginForm" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:260px"></el-input>
+          <el-button style="float:right">验证码</el-button>
+        </el-form-item>
+        <el-form-item class="checkbox">
+          <el-checkbox v-model="checked"></el-checkbox>
+          <span>
+            我已阅读并同意
+            <el-link type="primary" :underline="false">用户协议</el-link>和
+            <el-link type="primary" :underline="false">隐私条款</el-link>
+          </span>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary" @click="login">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
+
   data () {
+    const checkMoblie = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('请输入11位手机号'))
+      }
+    }
     return {
-      checked: true
+      checked: true,
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMoblie, trigger: 'blur' }
+
+        ],
+        code: [
+          { required: true, message: '请输入验正码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验正码', trigger: 'blur' }
+
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          // 校验成功，进行登录
+          this.axios
+            .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations')
+            .then((res) => {
+              const { data } = res.data
+              console.log(data)
+            })
+            .catch(() => {
+              this.$message.error('错了哦，这是一条错误消息')
+            })
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped lang='less'>
-    .login-container{
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
-        .login{
-            width: 420px;
-            height: 340px;
-            position: absolute;
-            top:50%;
-            left: 50%;
-            transform: translate(-50%,-50%);
-            img{
-                width: 220px;
-                display: block;
-                margin: 16px auto;
-            }
-            .el-checkbox {
-                margin-right: 10px
-            }
-            .checkbox{
-                margin-bottom: 10px;
-            }
-
-        }
-
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login {
+    width: 420px;
+    height: 340px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 220px;
+      display: block;
+      margin: 16px auto;
     }
+    .el-checkbox {
+      margin-right: 10px;
+    }
+    .checkbox {
+      margin-bottom: 10px;
+    }
+  }
+}
 </style>
