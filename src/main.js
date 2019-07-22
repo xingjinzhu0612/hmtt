2882396e83b945f6fae08ead890d1bae68ced24a
xingjinzhu0612 <1018715459@qq.com>
2019-07-22 09:20:13 +0800

    修改入口文件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import router from '@/router'
+import router from './router/router'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
